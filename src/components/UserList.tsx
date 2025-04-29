4c74f13055bc210b7c357df02f72cd9b6dfa99dd
emarishkin <ee.marishkin@yandex.ru>
2025-04-29 16:31:08 +0400

    2

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,10 +1,15 @@
-import { FC, useEffect, useState } from "react";
+import { ChangeEvent, FC, useEffect, useState } from "react";
 import { User } from "../Types";
 import { fetchUsers } from "../userService";
 import { UserCard } from "./UserCard";
 
 export const UserList: FC = () => {
   const [users, setUsers] = useState<User[]>([]);
+  const [newUserName,setNewUserName] = useState<string>('')
+  const [newUserEmail,setNewUserEmail] = useState<string>('')
+
+
+
 
   useEffect(() => {
     fetchUsers().then(setUsers);
@@ -14,11 +19,47 @@ export const UserList: FC = () => {
     setUsers(users.filter(user => user.id !== id));
   };
 
+  const ChangeName = (e:ChangeEvent<HTMLInputElement>) =>{
+    setNewUserName(e.target.value) 
+  }
+  const ChangeEmail = (e:ChangeEvent<HTMLInputElement>) =>{
+    setNewUserEmail(e.target.value) 
+  }
+
+  const AddUser = () =>{
+    if(newUserName!==''&&newUserEmail!==''){
+      const newUser = {
+        id:Date.now(),
+        name:newUserName,
+        email:newUserEmail
+      }
+      setUsers([...users,newUser])
+      setNewUserName('')
+      setNewUserEmail('')
+    }
+  }
+
   return (
     <div>
       {users.map(user => (
         <UserCard key={user.id} user={user} onDelete={handleDelete} />
       ))}
+
+      <input 
+      type="text"
+      value={newUserName}
+      placeholder="Введите имя"
+      onChange={ChangeName}
+      />
+
+      <input 
+      type="text"
+      value={newUserEmail}
+      placeholder="Введите email"
+      onChange={ChangeEmail}
+      />
+
+      <button onClick={AddUser}>добавить нового пользователя</button>
     </div>
   );
 };
--- src/userService.ts
+++ src/userService.ts
@@ -2,9 +2,7 @@ import { User } from "./Types";
 
 export const fetchUsers = async (): Promise<User[]> => {
     const response = await fetch("https://jsonplaceholder.typicode.com/users");
-    if (!response.ok) {
-      throw new Error("Ошибка загрузки пользователей");
-    }
+   
     const data = await response.json();
     return data;
   };
